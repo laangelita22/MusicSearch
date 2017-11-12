e51d0c092d79fe460ba8d3d2f8a716aecc7844dd
Angel Arciga <31705013+laangelita22@users.noreply.github.com>
2017-11-11 21:32:53 -0700

    Music Search functioning.

---
+++ app.js
@@ -0,0 +1,35 @@
+var MusicSearch = (function () {
+
+  
+    var songs = [];
+
+    function MusicSearch() {
+
+
+    };
+
+    MusicSearch.prototype.getResults = function (searchTerm, limit, callback) {
+
+        searchTerm = encodeURIComponent(searchTerm);
+        var apiUrl = `https://itunes.apple.com/search?term=${searchTerm}&limit=${limit}`;
+        var url = `http://bcw-getter.herokuapp.com/?url=` + encodeURIComponent(apiUrl);
+
+        $.getJSON(url, function (data) {
+            var songList = data.results.map(function (song) {
+                return {
+                    title: song.trackName,
+                    albumArt: song.artworkUrl60,
+                    artist: song.artistName,
+                    collection: song.collectionName,
+                    price: song.collectionPrice,
+                    preview: song.previewUrl
+                }
+            });
+            console.log("data:", data);
+            console.log("songlist", songList);
+            callback(songList);
+        });
+    };
+
+    return MusicSearch;
+})();
